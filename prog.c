2139fc23478382c29b5ca23495189701449ccfe9
Colby Skeggs <skeggsc@catlin.edu>
2014-10-18 14:10:37 -0700

    At 2053.

--- prog.c
+++ prog.c
@@ -1,3 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <dlfcn.h>
+
 typedef int (*and_eq)();
 
 typedef struct {
@@ -6,21 +13,14 @@ typedef struct {
 	void *V;
 } W;
 
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <dlfcn.h>
-
-#define struct(z, x, y) FILE*q = fopen("t.c", "w"); if (q == NULL) exit(2); E(%cinclude "prog.c"\n extern void bitand() __attribute__((constructor)); \n, 35, 9); z fclose(q); if (system(CC " -DCC= -shared t.c -o " #x " " #y)) exit(3); { char *p = "./" #x; main(127, &p - 1); }
+#define struct(z, x, y) FILE*q = fopen("t.c", "w"); E(%cinclude "prog.c"\n extern void bitand() __attribute__((constructor)); \n, 35, 9); z fclose(q); if (system(CC " -DCC= -shared t.c -o " #x " " #y)) exit(3); { dlopen("./" #x, RTLD_LAZY); }
 #define E(x, v, w) fprintf(q, #x, v, w);
 #define H "THE LEMONLORD"
-#define Q (uintptr_t)
+#define Q (intptr_t)&
 #define O(x) = (J(x)(V.V) - 32 / x) / 2,
 #define R(x, y) E(J(3)(%d, %d, B(0) == x ? "{" #y "} " : " " #y); , o + 12, a += (3 + 5*x) | 1 )
 #define A(x) ((W *)x)->
-#define Z "%4d%5d"
+#define Z "%5d"
 #define B(x) V.U[x]
 #define J(x) V.T[x]
 #define restrict sprintf
@@ -36,7 +36,7 @@ void xor_eq() {
 		return;
 	}
 	struct(
-	E(int k() { V = *(W *)%td; char *y = (char*) %td; , Q &V, Q M)
+	E(int k() { V = *(W *)%td; char *y = (char*) %td; , Q V, Q M)
 	E(int x = %d; , a, a)
 	char *x = "\0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0{                                } \0{                                } \0{                                } \0{                                } \0{  {HP} {MP}          {HP} {MP}  } \0{                                } \0{      O                  O      } \0{     /|-}              {-|\\\\     } \0{      |                  |      } \0{     / \\\\                / \\\\     } \0{                                } \0{                                } \0{                                } \0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0";
 	while (*++x) for (E(J(3)(%d, x, "%s"); , b++, x)*++x; );
@@ -44,8 +44,8 @@ void xor_eq() {
 	R(1, REGENERATE)
 	R(2, HEAL)
 	E(J(3)(%d, %d - strlen(y) / 2, y); char d[99]; , o+2, a - 8)
-	E(restrict(d, Z "          " Z, B(1), B(3), B(2), B(4)); J(3)(%d, %d, d); , o + 4, a - 23)
-	E(J(4)(); } void bitand() { A(%td)T[14] = k; } , Q &V, V)
+	E(restrict(d, Z Z "         " Z Z, B(1), B(3), B(2), B(4)); J(3)(%d, %d, d); , o + 4, a - 24)
+	E(J(4)(); } void bitand() { A(%td)T[14] = k; } , Q V, V)
 	, volatile, )
 	xor_eq();
 }
@@ -73,14 +73,14 @@ int x() {
 	X(< 9 ? 0 : B(2) < 20 ? B(4) < 2 ? B(3) >= 1 : 2 : B(4) < 5 && S(3) != 1 && !(B(3) < 1) ? 1 : S(8) > 6 && (B(3) || B(4) > 1) ? 1 + (B(4) > 9 || B(3) < 1) : 0, 1, They, if (B(3) < D) { D = B(3); } B(3) -= D, steal)
 }
 
-int v(int *p, and_eq *P) {
+int w(int *p, and_eq *P) {
 	int r = 9;
 	struct(
 	E(int f(int x) { switch(x) { , 0, 0)
 	while (*p) {
-		E(case %d: return ((and_eq) %td)(); , r += *(p++), Q *(P++));
+		E(case %d: return ((and_eq) %td)(); , r += *(p++), Q **(P++));
 	}
-	E(} return 1; } void bitand() { A(%td) T[13] = f; } , Q &V, 1)
+	E(} return 1; } void bitand() { A(%td) T[13] = f; } , Q V, 1)
 	, thread_local, )
 	return p[-2];
 }
@@ -89,43 +89,31 @@ int u() {
 	return B(5) = 1;
 }
 
-void bitor() {
-	xor_eq();
-	int g = J(5)();
-	if (g & (~((1+~1U) >> 1))) {
-		B(0) = (B(0) + g + 3) % 3;
-	} else {
-		J(13) || v((int[]) { 1, 3, 14, 5, 49, 32, 0} , (and_eq[]) { x, x, u, x, u, u} );
-		J(13)(g);
-	}
-}
-
-void and() {
+int main(int j, char **v) {
+	srand(time(0));
 	struct(
-	E(#include <ncurses/ncurses.h>\n void F() { endwin(); puts("Play again?"); } int g() { int o = getch(); return o == KEY_LEFT ? -1 : o == KEY_RIGHT ? -2 : o; } void bitand() {  and_eq *G = A(%td) T - 1; , Q &V, 8);
+	E(#include <ncurses/ncurses.h>\n void F() { endwin(); puts("Play again?"); } int g() { int o = getch(); return o == KEY_LEFT ? -1 : o == KEY_RIGHT ? -2 : o; } void bitand() {  and_eq *G = A(%td) T - 1; , Q V, 8);
 	char *i = "\0keypad\0getmaxx\0getmaxy\0mvaddstr\0refresh\0g\0cbreak\0noecho\0nonl\0";
+	memcpy(V.U, (int[6]) { 0, 30, 50, 5, 10, 0} , 6 * sizeof(int));
 	while (*++i) {
 		E(*++G = (and_eq) %s; , i, i);
 		while (*++i);
 	}
-	E(initscr(); A(%td)V = stdscr; atexit(F); } , Q &V, i)
+	E(initscr(); A(%td)V = stdscr; atexit(F); } , Q V, i)
 	, static_assert, -lncurses)
-	int j = 6;
-	while (j < 9) {
+	for (j = 6; j < 9; ) {
 		J(j++)();
 	}
 	J(0)(V.V, 1);
-	for (; !B(5); bitor());
-}
-
-int main(int c, char **v) {
-/*int B(0) = 0, B(1) = 30, B(2) = 50, B(3) = 5, B(4) = 10, B(5) = 0; */
-	/* do we need srand(time(NULL))? */
-	if (v[1]) { /* sanity test */
-		dlopen(v[1], RTLD_LAZY);
-	} else {
-		memcpy(V.U, (int[]) { 0, 5, 5, 5, 10, 0} , 6 * sizeof(int));
-		and();
+	for (; !B(5); ) {
+		xor_eq();
+		j = J(5)();
+		if (j & (~((1+~1U) >> 1))) {
+			B(0) = (B(0) + j + 3) % 3;
+		} else {
+			J(13) || w((int[]) { 1, 3, 14, 5, 49, 32, 0} , (and_eq[]) { x, x, u, x, u, u} );
+			J(13)(j);
+		}
 	}
 	return 0;
 }

a71d046e9bed47bde408b5982047bded5d29bffb
Colby Skeggs <skeggsc@catlin.edu>
2014-10-18 15:27:14 -0700

    Messed up the syntax.

--- prog.c
+++ prog.c
@@ -31,21 +31,29 @@ W V;
 
 void xor_eq() {
 	int a O(1) b O(2) o = b;
-	if (J(14)) {
-		J(14)();
-		return;
-	}
-	struct(
-	E(int k() { V = *(W *)%td; char *y = (char*) %td; , Q V, Q M)
+	if (J(14)) { J(14)();
+	return;
+}
+
+struct(E(
+
+int k() {
+	V = *(W *)%td;
+	char *y = (char*) %td; , Q V, Q M)
 	E(int x = %d; , a, a)
-	char *x = "\0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0{                                } \0{                                } \0{                                } \0{                                } \0{  {HP} {MP}          {HP} {MP}  } \0{                                } \0{      O                  O      } \0{     /|-}              {-|\\\\     } \0{      |                  |      } \0{     / \\\\                / \\\\     } \0{                                } \0{                                } \0{                                } \0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0";
-	while (*++x) for (E(J(3)(%d, x, "%s"); , b++, x)*++x; );
-	R(0, ATTACK)
-	R(1, REGENERATE)
-	R(2, HEAL)
+		char *x = "\0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0{                                } \0{                                } \0{                                } \0{                                } \0{  {HP} {MP}          {HP} {MP}  } \0{                                } \0{      O                  O      } \0{     /|-}              {-|\\\\     } \0{      |                  |      } \0{     / \\\\                / \\\\     } \0{                                } \0{                                } \0{                                } \0; ; ; ; ; ; ; ; ;; ; ; ; ; ; ; ; ; \0";
+	while (*++x) for (E(J(3)(%d, x, "%s"); , b++, x)*++x; ); {
+		R(0, ATTACK)
+		R(1, REGENERATE)
+		R(2, HEAL)
+	}
 	E(J(3)(%d, %d - strlen(y) / 2, y); char d[99]; , o+2, a - 8)
 	E(restrict(d, Z Z "         " Z Z, B(1), B(3), B(2), B(4)); J(3)(%d, %d, d); , o + 4, a - 24)
-	E(J(4)(); } void bitand() { A(%td)T[14] = k; } , Q V, V)
+	E(J(4)();
+}
+
+void bitand() {
+	A(%td)T[14] = k; } , Q V, V)
 	, volatile, )
 	xor_eq();
 }
@@ -70,29 +78,27 @@ int x() {
 	int u = 0, D;
 	X(, 0, You, , gain)
 	sleep(1);
-	X(< 9 ? 0 : B(2) < 20 ? B(4) < 2 ? B(3) >= 1 : 2 : B(4) < 5 && S(3) != 1 && !(B(3) < 1) ? 1 : S(8) > 6 && (B(3) || B(4) > 1) ? 1 + (B(4) > 9 || B(3) < 1) : 0, 1, They, if (B(3) < D) { D = B(3); } B(3) -= D, steal)
-}
-
-int w(int *p, and_eq *P) {
+	X(< 9 ? 0 : B(2) < 20 ? B(4) < 2 ? B(3) >= 1 : 2 : B(4) < 5 && S(3) != 1 && !(B(3) < 1) ? 1 : S(8) > 6 && (B(3) || B(4) > 1) ? 1 + (B(4) > 9 || B(3) < 1) : 0, 1, They, if (B(3) < D) { D = B(3); } B(3) -= D, steal) } int w(int *p, and_eq *P) {
 	int r = 9;
 	struct(
 	E(int f(int x) { switch(x) { , 0, 0)
 	while (*p) {
-		E(case %d: return ((and_eq) %td)(); , r += *(p++), Q **(P++));
-	}
-	E(} return 1; } void bitand() { A(%td) T[13] = f; } , Q V, 1)
-	, thread_local, )
-	return p[-2];
-}
-
-int u() {
+		E(case %d: return ((and_eq) %td)(); , r += *(p++), Q **(P++)); }
+		E(} return 1; } void bitand() { A(%td) T[13] = f; } , Q V, 1)
+		, thread_local, )
+		return p[-2];
+	} int u() {
 	return B(5) = 1;
 }
 
 int main(int j, char **v) {
 	srand(time(0));
 	struct(
-	E(#include <ncurses.h>\n void F() { endwin(); puts("Play again?"); } int g() { int o = getch(); return o == KEY_LEFT ? -1 : o == KEY_RIGHT ? -2 : o; } void bitand() {  and_eq *G = A(%td) T - 1; , Q V, 8);
+	E(#include <ncurses.h>\n void F() { endwin(); puts("Play again?"); } int g() { int o = getch(); return o == KEY_LEFT ? -1 : o == KEY_RIGHT ? -2 : o;
+}
+
+void bitand() {
+	and_eq *G = A(%td) T - 1; , Q V, 8);
 	char *i = "\0keypad\0getmaxx\0getmaxy\0mvaddstr\0refresh\0g\0cbreak\0noecho\0nonl\0";
 	memcpy(V.U, (int[6]) { 0, 30, 50, 5, 10, 0} , 6 * sizeof(int));
 	while (*++i) {
